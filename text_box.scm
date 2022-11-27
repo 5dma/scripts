5e761a081304fedd75f1f44e1fc11beb1b29970d
5dma <mark@lautman.net>
2022-11-27 18:03:15 -0500

    Capture current image.

--- text_box.scm
+++ text_box.scm
@@ -1,15 +1,15 @@
-(define (script-fu-text-box inText inFont inFontSize inTextColor)
+(define (script-fu-text-box image drawable inText inFont inFontSize inTextColor)
 
 (let*
-(
-	(myimage (car (gimp-image-new 100 100 RGB)))
-	;(mylayer (car (gimp-layer-new myimage 50 50 RGB "background" 100  LAYER-MODE-NORMAL-LEGACY)))
-	(mylayer (car (gimp-layer-new myimage 50 50 RGB-IMAGE "background" 50  1)))
-	(textlayer (car (gimp-text-layer-new myimage inText inFont inFontSize UNIT-PIXEL)))
-)	
-(gimp-image-insert-layer myimage mylayer 0 -1)
-(gimp-image-insert-layer myimage textlayer 0 -1)
-(gimp-display-new myimage)
+	(
+		;(myimage (car (gimp-image-new 100 100 RGB)))
+		(mylayer (car (gimp-layer-new image 50 50 RGB-IMAGE "background" 50  1)))
+		;(textlayer (car (gimp-text-layer-new myimage inText inFont inFontSize UNIT-PIXEL)))
+	)	
+	;(gimp-item-transform-scale image 0 0 100 100)
+	(gimp-image-insert-layer image mylayer 0 -1)
+	;(gimp-image-insert-layer myimage textlayer 0 -1)
+	;(gimp-display-new myimage)
 )
 
 
@@ -27,6 +27,8 @@
       2009, the GIMP Documentation Team"        ;copyright notice
     "October 27, 1997"                          ;date created
     ""                     ;image type that the script works on
+  	SF-IMAGE       "The image"   0
+  	SF-DRAWABLE    "The drawable"   0
     SF-STRING      "Text"          "Text Box"   ;a string variable
     SF-FONT        "Font"          "DejaVu Sans Bold"    ;a font variable
     SF-ADJUSTMENT  "Font size"     '(39 1 1000 1 10 0 1)
